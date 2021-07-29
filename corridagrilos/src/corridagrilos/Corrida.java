04c0e0dde0431a9c5f276f9da41564e37691e8e3
Thiago_Amaro <thiagoamaro010399@gmail.com>
2021-07-28 21:14:54 -0300

    Reorganização das variavéis para uma classe não static

---
+++ corridagrilos/src/corridagrilos/Corrida.java
@@ -0,0 +1,12 @@
+package corridagrilos;
+
+public class Corrida {
+	int numGrilos;//numero de Grilos
+    int distancia;//distancia da corrida
+    float intervalo;//intervalo em segundos entre os pulos
+    int maximoPulo;//distancia maxima por pulo
+    
+    public Corrida() {
+    	
+    }
+}
--- corridagrilos/src/corridagrilos/Main.java
+++ corridagrilos/src/corridagrilos/Main.java
@@ -1,13 +1,10 @@
 package corridagrilos;
 
 public class Main {
-	int numGrilos;//numero de Grilos
-    int distancia;//distancia da corrida
-    float intervalo;//intervalo em segundos entre os pulos
-    int maximoPulo;//distancia maxima por pulo
+	static private Corrida corrida; 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		corrida = new Corrida();
 	}
 
 }
