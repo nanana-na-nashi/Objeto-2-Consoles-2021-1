0aa2ef09d733cd2f28452e42e21d71f8d7735cf8
ericksalvador <nanashous@gmail.com>
2021-07-28 21:03:43 -0300

    criação das variáveis

--- corridagrilos/src/corridagrilos/Main.java
+++ corridagrilos/src/corridagrilos/Main.java
@@ -1,7 +1,10 @@
 package corridagrilos;
 
 public class Main {
-
+	int numGrilos;//numero de Grilos
+    int distancia;//distancia da corrida
+    float intervalo;//intervalo em segundos entre os pulos
+    int maximoPulo;//distancia maxima por pulo
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
