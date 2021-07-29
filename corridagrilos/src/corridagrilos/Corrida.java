7c95bde22021f19d9ab76eeb1f8bbcbdc2c976a6
Thiago_Amaro <thiagoamaro010399@gmail.com>
2021-07-28 21:48:03 -0300

    instâciação das Threads dos Grilos

--- corridagrilos/src/corridagrilos/Corrida.java
+++ corridagrilos/src/corridagrilos/Corrida.java
@@ -1,12 +1,18 @@
 package corridagrilos;
 
 public class Corrida {
-	int numGrilos;//numero de Grilos
-    int distancia;//distancia da corrida
-    float intervalo;//intervalo em segundos entre os pulos
-    int maximoPulo;//distancia maxima por pulo
+	int numGrilos = 4;//numero de Grilos
+	public static int distancia;//distancia da corrida
+	public static float intervalo;//intervalo em segundos entre os pulos
+	public static int maximoPulo;//distancia maxima por pulo
     
+    Grilo[] grilos;
     public Corrida() {
-    	
+		System.out.println("A corrida Começou!");
+    	grilos = new Grilo[numGrilos];
+		for(int i = 0; i <numGrilos;i++) {
+			grilos[i] = new Grilo(i+1);
+			grilos[i].start();
+		}
     }
 }
--- corridagrilos/src/corridagrilos/Grilo.java
+++ corridagrilos/src/corridagrilos/Grilo.java
@@ -1,7 +1,11 @@
 package corridagrilos;
 
 public class Grilo extends Thread {
+	int numero;
+	public Grilo(int numero) {
+		this.numero = numero;
+	}
     public void run(){
-        System.out.println("thread is running...");
+        System.out.println("O grilo "+numero+" começou a pular!!");
     }
 }
