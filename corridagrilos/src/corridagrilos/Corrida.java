9fbd52bdd70775358f3371ca9f7f76e07bc1455e
Thiago_Amaro <thiagoamaro010399@gmail.com>
2021-07-28 22:35:29 -0300

    Reconfiguração das variaveis

--- corridagrilos/src/corridagrilos/Corrida.java
+++ corridagrilos/src/corridagrilos/Corrida.java
@@ -2,9 +2,8 @@ package corridagrilos;
 
 public class Corrida {
 	int numGrilos = 4;//numero de Grilos
-	public static int distancia;//distancia da corrida
-	public static float intervalo;//intervalo em segundos entre os pulos
-	public static int maximoPulo;//distancia maxima por pulo
+	public static int distancia = 100;//distancia da corrida
+	public static int maximoPulo = 20;//distancia maxima por pulo
     
     Grilo[] grilos;
     public Corrida() {
@@ -13,6 +12,7 @@ public class Corrida {
 		for(int i = 0; i <numGrilos;i++) {
 			grilos[i] = new Grilo(i+1);
 			grilos[i].start();
-		}
+		}	
+		
     }
 }
