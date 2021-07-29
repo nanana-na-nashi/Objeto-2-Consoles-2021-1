3a2f5e03469eece91751a7b9b91f6681d0bb3dde
jadhyscott <jadhysmb@gmail.com>
2021-07-28 22:38:15 -0300

    Sistema de pulos da classe grilo

--- corridagrilos/src/corridagrilos/Grilo.java
+++ corridagrilos/src/corridagrilos/Grilo.java
@@ -1,11 +1,24 @@
 package corridagrilos;
 
+import java.util.Random;
+
 public class Grilo extends Thread {
-	int numero;
-	public Grilo(int numero) {
-		this.numero = numero;
-	}
+    int numero;
+    int distancia;
+    int pulos = 0;
+    Random random;
+    public Grilo(int numero) {
+        this.numero = numero;
+        random = new Random();
+    }
     public void run(){
-        System.out.println("O grilo "+numero+" começou a pular!!");
+        System.out.println("O grilo "+numero+" comeï¿½ou a pular!!");
+        while(distancia<Corrida.distancia) {
+            int pulo = random.nextInt(Corrida.maximoPulo);
+            distancia += pulo;
+            pulos++;
+            System.out.println("O grilo "+numero+" pulou "+ pulo+ " cm! Ja percorreu "+ distancia +" cm");
+        }
+        System.out.println("O grilo "+numero+" pulou Chegou na linha de chegada com "+pulos +" pulos!");
     }
 }
