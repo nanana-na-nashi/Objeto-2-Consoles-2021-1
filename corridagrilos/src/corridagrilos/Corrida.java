30f7c7e9b78c6e8009a84bba898d300ec6c94eba
nanana-na-nashi <nanashous@gmail.com>
2021-08-16 16:43:08 -0300

    Criação do Semáforo

--- corridagrilos/src/corridagrilos/Corrida.java
+++ corridagrilos/src/corridagrilos/Corrida.java
@@ -1,18 +1,21 @@
 package corridagrilos;
+import java.util.concurrent.Semaphore;
 
 public class Corrida {
 	int numGrilos = 4;//numero de Grilos
 	public static int distancia = 100;//distancia da corrida
 	public static int maximoPulo = 20;//distancia maxima por pulo
-    
+	
+	Semaphore semaphore;
     Grilo[] grilos;
     public Corrida() {
-		System.out.println("A corrida Começou!");
-    	grilos = new Grilo[numGrilos];
-		for(int i = 0; i <numGrilos;i++) {
-			grilos[i] = new Grilo(i+1);
-			grilos[i].start();
-		}	
-		
+    	semaphore = new Semaphore(1);
+
+        System.out.println("A corrida Começou!");
+        grilos = new Grilo[numGrilos];
+        for(int i = 0; i <numGrilos;i++) {
+            grilos[i] = new Grilo(i+1, semaphore);
+            grilos[i].start();
+        }
     }
 }
--- corridagrilos/src/corridagrilos/Grilo.java
+++ corridagrilos/src/corridagrilos/Grilo.java
@@ -1,15 +1,20 @@
 package corridagrilos;
 
 import java.util.Random;
+import java.util.concurrent.Semaphore;
 
 public class Grilo extends Thread {
     int numero;
     int distancia;
     int pulos = 0;
+    int time;
     Random random;
-    public Grilo(int numero) {
+    Semaphore semaphore;
+    public Grilo(int numero, Semaphore _semaphore) {
         this.numero = numero;
+        time = numero%3+1;//2,3,1,2,3,1
         random = new Random();
+        semaphore = _semaphore;
     }
     public void run(){
         System.out.println("O grilo "+numero+" come�ou a pular!!");
