a530582887eab329b50270a57053f378b9509ca0
jadhyscott <jadhysmb@gmail.com>
2021-08-16 16:48:45 -0300

    Verificação da corrida
    Verifica se a corrida acabou, se todos os grilos chegaram

--- corridagrilos/src/corridagrilos/Corrida.java
+++ corridagrilos/src/corridagrilos/Corrida.java
@@ -2,9 +2,11 @@ package corridagrilos;
 import java.util.concurrent.Semaphore;
 
 public class Corrida {
-	int numGrilos = 4;//numero de Grilos
+	int numGrilos = 6;//numero de Grilos
 	public static int distancia = 100;//distancia da corrida
 	public static int maximoPulo = 20;//distancia maxima por pulo
+	public static int posicao = 1;
+    public static int vencedor;
 	
 	Semaphore semaphore;
     Grilo[] grilos;
@@ -17,5 +19,26 @@ public class Corrida {
             grilos[i] = new Grilo(i+1, semaphore);
             grilos[i].start();
         }
+        int left = numGrilos;
+        while(left!=0) {
+            left = numGrilos;
+            for(Grilo g : grilos) {
+                if(g.GetDist()>= distancia)
+                    left--;
+            }
+        }
+        System.out.println("A corrida Acabou!");
+
+        int[] pulos = new int[3];
+        int[] dist = new int[3];
+        for(Grilo g:grilos) {
+            pulos[g.GetTime()-1]+=g.GetPulos();
+            dist[g.GetTime()-1]+=g.GetDist();
+        }
+
+        for(int i =0; i<3;i++) {
+            System.out.println("Time "+(i+1)+": Total de pulos: "+pulos[i]+" - Distância Percorrida: "+dist[i]);
+        }
+        System.out.println("Time "+vencedor +" foi o vencedor!");
     }
 }
